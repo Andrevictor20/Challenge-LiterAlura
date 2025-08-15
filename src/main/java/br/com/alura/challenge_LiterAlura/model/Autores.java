94a9de17199ea985b74d332c87dc0ebe118f6077
André Victor Macedo Pereira <victorandre280@gmail.com>
2025-08-15 20:10:29 -0300

    Criação das classes (model) Autores e livro

---
+++ src/main/java/br/com/alura/challenge_LiterAlura/model/Autores.java
@@ -0,0 +1,8 @@
+package br.com.alura.challenge_LiterAlura.model;
+
+public class Autores {
+    private Integer anoNascimento;
+    private Integer anoFalecimento;
+    private String nome;
+
+}
---
+++ src/main/java/br/com/alura/challenge_LiterAlura/model/Livro.java
@@ -0,0 +1,15 @@
+package br.com.alura.challenge_LiterAlura.model;
+
+import java.util.List;
+
+public class Livro {
+
+    private String id;
+    private String titulo;
+    private List<String> assuntos;
+    private List<Autores> autores;
+    private List<String> resumos;
+    private List<String> idiomas;
+    private Integer downloads;
+
+}
